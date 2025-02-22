0d87f090554d9d14a056875a9f9477eb48b589ca
Daniel Cox <Daniel.Cox@estes-express.com>
2025-02-21 19:37:53 -0500

    added material, tailwind; reverted scss to css

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatSlideToggleModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'dnn-crew';
